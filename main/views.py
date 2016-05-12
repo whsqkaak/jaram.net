cfc0f71fd72abc66dcabb304133379729044877e
Jade Kim <jinyong@jadekim.kr>
2016-05-12 10:57:30 +0900

    [추가] 메인페이지 추가

--- main/views.py
+++ main/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.views.generic import TemplateView
 
 
 class MainView(TemplateView):
-    pass
+    template_name = 'main.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+
+
+class LoginView(TemplateView):
+    template_name = 'login.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
