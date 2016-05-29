c4fdedc024aabdd00863535f1f72cd4d3d7ad401
Jade Kim <jinyong@jadekim.kr>
2016-05-29 17:29:31 +0900

    [추가] 스터디 목록 화면 추가

--- board/models.py
+++ board/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
 from main.models import Member
 
 
 class BaseBoardModel(models.Model):
     writer = models.ForeignKey(Member)
-    write_date = models.DateTimeField(_('작성일'))
+    write_date = models.DateTimeField(_('작성일'), null=False, blank=False, default=timezone.now)
     title = models.CharField(_('제목'), max_length=255, null=True, blank=True, default='')
     content = models.TextField(_('내용'), null=False, blank=False)
 
@@ -22,6 +23,15 @@ class PlayStorming(BaseBoardModel):
         verbose_name_plural = _('플레이스토밍')
 
 
+class PlayStormingComment(BaseBoardModel):
+    board = models.ForeignKey(PlayStorming)
+
+    class Meta:
+        ordering = ['-write_date']
+        verbose_name = _('플레이스토밍 댓글')
+        verbose_name_plural = _('플레이스토밍 댓글')
+
+
 class Seminar(BaseBoardModel):
     class Meta:
         ordering = ['-write_date']
@@ -29,8 +39,26 @@ class Seminar(BaseBoardModel):
         verbose_name_plural = _('세미나')
 
 
+class SeminarComment(BaseBoardModel):
+    board = models.ForeignKey(Seminar)
+
+    class Meta:
+        ordering = ['-write_date']
+        verbose_name = _('세미나 댓글')
+        verbose_name_plural = _('세미나 댓글')
+
+
 class EventReport(BaseBoardModel):
     class Meta:
         ordering = ['-write_date']
         verbose_name = _('행사보고')
         verbose_name_plural = _('행사보고')
+
+
+class EventReportComment(BaseBoardModel):
+    board = models.ForeignKey(EventReport)
+
+    class Meta:
+        ordering = ['-write_date']
+        verbose_name = _('행사보고 댓글')
+        verbose_name_plural = _('행사보고 댓글')
