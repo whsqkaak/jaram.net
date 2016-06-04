2fda63bc86caa05801f211b583d212788b769674
Jade Kim <jinyong@jadekim.kr>
2016-06-04 15:35:38 +0900

    [추가] 스터디 상세보기 화면 추가

--- study/models.py
+++ study/models.py
@@ -7,12 +7,16 @@ from main.models import Member
 class Study(models.Model):
     leader = models.ForeignKey(Member, related_name='created_study_set')
     members = models.ManyToManyField(Member, related_name='study_set')
-    image = models.ImageField(_('대표 이미지'), null=False, blank=False)
-    title = models.CharField(_('스터디 이름'), max_length=32, null=False, blank=False)
+    image = models.ImageField(_('대표 이미지'), upload_to='study/main_image/', null=False, blank=False)
+    name = models.CharField(_('스터디 이름'), max_length=32, null=False, blank=False)
     description = models.CharField(_('간략한 설명'), max_length=64, null=False, blank=False)
     content = models.TextField(_('계획, 기간 등 상세한 설명'), null=False, blank=False)
     date = models.DateField(_('스터디 등록일'), null=False, blank=False, default=timezone.now)
+    attachment = models.FileField(_('첨부 파일'), upload_to='study/attachment/', null=True, blank=True)
     is_active = models.BooleanField(_('진행 여부'), null=False, blank=False, default=False)
+
+    def __unicode__(self):
+        return self.name
 
     class Meta:
         ordering = ['-date']
@@ -22,8 +26,14 @@ class Study(models.Model):
 
 class StudyReport(models.Model):
     writer = models.ForeignKey(Member)
+    study = models.ForeignKey(Study)
     write_date = models.DateTimeField(_('작성일'), null=False, blank=False, default=timezone.now)
+    title = models.CharField(_('제목'), max_length=255, null=False, blank=False)
     content = models.TextField(_('내용'), null=False, blank=False)
+    attachment = models.FileField(_('첨부 파일'), upload_to='study/report/attachment/', null=True, blank=True)
+
+    def __unicode__(self):
+        return self.study.name + ' - ' + self.title
 
     class Meta:
         ordering = ['-write_date']
--- study/views.py
+++ study/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 from main.util import create_response
 from study.models import Study
@@ -11,3 +12,36 @@ class StudyListView(TemplateView):
         response = create_response(request)
         response['page'] = Study.objects.filter(is_active=True).all()
         return render(request, self.template_name, response)
+
+
+class StudyDetailView(TemplateView):
+    template_name = 'detail.html'
+
+    def get(self, request, *args, **kwargs):
+        response = create_response(request)
+        try:
+            study = Study.objects.get(pk=kwargs.get('id'))
+        except ObjectDoesNotExist:
+            return redirect('/study?error=존재하지 않는 스터디입니다.')
+
+        response['study'] = study
+        response['reports'] = study.studyreport_set.all()
+        response['members'] = study.members.all()
+
+        return render(request, self.template_name, response)
+
+
+class StudyReportView(TemplateView):
+    template_name = 'report.html'
+
+    def get(self, request, *args, **kwargs):
+        response = create_response(request)
+        try:
+            response['study'] = Study.objects.get(pk=kwargs.get('id'))
+        except ObjectDoesNotExist:
+            return redirect('/study?error=존재하지 않는 스터디입니다.')
+
+        return render(request, self.template_name, response)
+
+    def post(self, request, *args, **kwargs):
+        pass
