82beebf91e29c9bcae4c3b82c5c0bc2f3184ecd1
Jade Kim <jinyong@jadekim.kr>
2016-06-04 15:35:38 +0900

    [추가] 스터디 상세보기 화면 추가

--- study/urls.py
+++ study/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
-from study.views import StudyListView
+from study.views import StudyListView, StudyDetailView, StudyReportView
 
 urlpatterns = [
     url(r'^$', StudyListView.as_view(), name='study'),
+    url(r'^(?P<id>\d+)/?$', StudyDetailView.as_view(), name='study_detail'),
+    url(r'^(?P<id>\d+)/report/?$', StudyReportView.as_view(), name='study_report'),
 ]
