94f8c4aa18eb874145a56f72567518c68f841c8f
Jongheon Kim <simba3447@gmail.com>
2016-07-12 16:34:45 +0900

    [추가]공지사항, 재학생 게시판, 졸업생 게시판 작성 [수정]게시글 썸네일 필드 제거

--- board/urls.py
+++ board/urls.py
@@ -1,5 +1,6 @@
 from board.views import PlayStormingView, EventReportView, SeminarListView, MakePostView, \
-    PlayStormingListView, SeminarDetailView, PlayStormingDetailView
+    PlayStormingListView, SeminarDetailView, PlayStormingDetailView, AnnouncementListView, GraduatingBoardListView, \
+    StudentBoardListView, AnnouncementDetailView, GraduatingBoardDetailView, StudentBoardDetailView
 from django.conf.urls import url
 
 urlpatterns = [
@@ -8,5 +9,11 @@ urlpatterns = [
     url(r'^seminar/(?P<id>\d+)/?$', SeminarDetailView.as_view(), name='seminar_detail'),
     url(r'^playstorming/(?P<id>\d+)/?$', PlayStormingDetailView.as_view(), name='playstorming_detail'),
     url(r'^event_report$', EventReportView.as_view(), name='event_report'),
-    url(r'^write_post$', MakePostView.as_view(), name='write_post')
+    url(r'^write_post$', MakePostView.as_view(), name='write_post'),
+    url(r'^announcement$', AnnouncementListView.as_view(), name='announcement'),
+    url(r'^announcement/(?P<id>\d+)/?$', AnnouncementDetailView.as_view(), name='announcement_detail'),
+    url(r'^graduating$', GraduatingBoardListView.as_view(), name='graduating'),
+    url(r'^graduating/(?P<id>\d+)/?$', GraduatingBoardDetailView.as_view(), name='graduating_detail'),
+    url(r'^student$', StudentBoardListView.as_view(), name='student'),
+    url(r'^student/(?P<id>\d+)/?$', StudentBoardDetailView.as_view(), name='student_detail'),
 ]
