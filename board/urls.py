657b4fe89839875e388f8de9541ac33e71812c08
Jade Kim <jinyong@jadekim.kr>
2016-07-14 19:24:58 +0900

    [업데이트] 게시글을 각각 따로 테이블로 저장하는 것이 아니라 하나의 테이블에 저장하도록 변경

--- board/urls.py
+++ board/urls.py
@@ -1,26 +1,10 @@
-from board.views import EventReportView, SeminarListView, MakePostView, \
-    PlayStormingListView, SeminarDetailView, PlayStormingDetailView, AnnouncementListView, GraduatingBoardListView, \
-    StudentBoardListView, AnnouncementDetailView, GraduatingBoardDetailView, StudentBoardDetailView, SeminarDeleteView, \
-    PlayStormingDeleteView, AnnouncementDeleteView, StudentBoardDeleteView, GraduatingBoardDeleteView, EditPostView
+from board.views import PostListView, PostView, PostWriteView
 from django.conf.urls import url
 
 urlpatterns = [
-    url(r'^playstorming$', PlayStormingListView.as_view(), name='playstorming'),
-    url(r'^seminar$', SeminarListView.as_view(), name='seminar'),
-    url(r'^seminar/(?P<id>\d+)/?$', SeminarDetailView.as_view(), name='seminar_detail'),
-    url(r'^seminar/delete/(?P<id>\d+)/?$', SeminarDeleteView.as_view(), name='seminar_delete'),
-    url(r'^playstorming/(?P<id>\d+)/?$', PlayStormingDetailView.as_view(), name='playstorming_detail'),
-    url(r'^playstorming/delete/(?P<id>\d+)/?$', PlayStormingDeleteView.as_view(), name='playstorming_delete'),
-    url(r'^event_report$', EventReportView.as_view(), name='event_report'),
-    url(r'^write_post$', MakePostView.as_view(), name='write_post'),
-    url(r'^edit_post$', EditPostView.as_view(), name='edit_post'),
-    url(r'^announcement$', AnnouncementListView.as_view(), name='announcement'),
-    url(r'^announcement/(?P<id>\d+)/?$', AnnouncementDetailView.as_view(), name='announcement_detail'),
-    url(r'^announcement/delete/(?P<id>\d+)/?$', AnnouncementDeleteView.as_view(), name='announcement_delete'),
-    url(r'^graduating$', GraduatingBoardListView.as_view(), name='graduating'),
-    url(r'^graduating/(?P<id>\d+)/?$', GraduatingBoardDetailView.as_view(), name='graduating_detail'),
-    url(r'^graduating/delete/(?P<id>\d+)/?$', GraduatingBoardDeleteView.as_view(), name='graduating_delete'),
-    url(r'^student$', StudentBoardListView.as_view(), name='student'),
-    url(r'^student/(?P<id>\d+)/?$', StudentBoardDetailView.as_view(), name='student_detail'),
-    url(r'^student/delete/(?P<id>\d+)/?$', StudentBoardDeleteView.as_view(), name='student_delete')
+    url(r'^write/?$', PostWriteView.as_view(), name='post_write'),
+    url(r'^modify/(?P<id>\d+)/?$', PostWriteView.as_view(), name='post_modify'),
+    url(r'^delete/(?P<id>\d+)/?$', PostWriteView.as_view(), name='post_delete'),
+    url(r'^(?P<name>\w+)/?$', PostListView.as_view(), name='post_list'),
+    url(r'^(?P<name>\w+)/(?P<id>\d+)/?$', PostView.as_view(), name='post_detail'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,10 +1,8 @@
+from board.models import Post, Board
 from django.shortcuts import render, redirect
-from django.views.generic import View, TemplateView
+from django.views.generic import TemplateView
 from main.util import create_response
-from board.models import Seminar, PlayStorming, Announcement, GraduatingBoard, \
-    StudentBoard
-from itertools import chain
-from operator import attrgetter
+
 
 class IntroView(TemplateView):
     template_name = 'intro.html'
@@ -19,24 +17,13 @@ class MainView(TemplateView):
     def get(self, request, *args, **kwargs):
         response = create_response(request)
 
-        new_page = []
-        new_page = chain(new_page, (Seminar.objects.order_by('-write_date').all()))
-        new_page = chain(new_page, (PlayStorming.objects.order_by('-write_date').all()))
-        new_page = chain(new_page, (GraduatingBoard.objects.order_by('-write_date').all()))
-        new_page = chain(new_page, (StudentBoard.objects.order_by('-write_date').all()))
-        new_page = sorted(new_page, key=attrgetter('write_date'), reverse=True)
-        response['new_page'] = new_page[:3]
+        notice = Board.objects.get(name='공지사항')
 
-        if(len(new_page) >= 1):
-            response['dir_0'] = new_page[0].__class__.__name__
-        if(len(new_page) >= 2):
-            response['dir_1'] = new_page[1].__class__.__name__
-        if(len(new_page) >= 3):
-            response['dir_2'] = new_page[2].__class__.__name__
+        response['recent_posts'] = Post.objects.exclude(board=notice) \
+                                      .filter(board__usable_group__in=request.user.groups.all()) \
+                                      .order_by('-write_date')[:3]
 
-        announce_page = Announcement.objects.all()
-        announce_page_important = Announcement.objects.filter(importance=True).all()
-        response['announce_page'] = (list(announce_page_important) + list(announce_page))[:3]
+        response['notice_posts'] = notice.post_set.order_by('-emphasis')[:3]
 
         return render(request, self.template_name, response)
 
